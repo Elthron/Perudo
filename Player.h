a0d65fc29d728fbce39cb6bc78e48bb5b76aec7b
Ben <benjamin.stks@gmail.com>
2016-08-25 17:28:13 +0100

    turned into abstract base class

--- Player.h
+++ Player.h
@@ -8,13 +8,14 @@
 //in this game a "bid" is a std::pair of ints: 4 2s -> std::pair<unsigned int,unsigned int> {4,2}
 typedef std::pair<unsigned int,unsigned int> Bid;
 
+//abstract base class to inherit humans and AIs from
 class Player
 {
 	public:
 		Player(int _client_id,unsigned int die_size);
 		
-		Bid takeTurn(Bid last_bid);
-		void sendBid(Bid bid);
+		virtual Bid takeTurn(Bid last_bid)=0;
+		virtual void sendBid(Bid bid)=0;
 		bool removeDice();
 		unsigned int countDice(unsigned int number) const;
 		
