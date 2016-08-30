64b0ccdb2bbfab4957264f3785be309efe505a60
Ben <benjamin.stks@gmail.com>
2016-08-30 22:30:07 +0100

    more operators + better value function

--- Bid.cpp
+++ Bid.cpp
@@ -1,11 +1,14 @@
+#include <cmath>
 #include "Bid.h"
 
-unsigned int getValue(const Bid& bid)
+unsigned int getBidValue(const Bid& bid)
 {
-	return bid->first* ((bid->second==1) ? 2 : 1);
+	//perform differently for a one
+	if(bid.second==1)return bid.first*10+bid.first-1;
+	else return 5.5*bid.first+bid.second-7.75-0.25*std::pow(-1,bid.first);
 }
 
-//note that operator< is the only one that need actually be defined as all the other operators can be written in terms of it
+//note that operator< is the only one that need actually be defined as all the other operators can be written in terms of it and its derivatives
 bool operator<(const Bid& lhs, const Bid& rhs)
 {
 	return getBidValue(lhs)<getBidValue(rhs);
