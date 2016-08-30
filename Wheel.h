93a21e3fd37a790c9f449de572d1e93b46c549b8
Ben <benjamin.stks@gmail.com>
2016-08-30 22:33:56 +0100

    converted to proper template class
    turns out I forget template classes can't have .cpp files as the linker(?) needs all the definitions

--- Wheel.h
+++ Wheel.h
@@ -10,25 +10,55 @@ template <typename T>
 class wheel
 {
 	public:
-		explicit wheel();
+		explicit wheel() {}
 		
 		class iterator : public std::iterator<std::bidirectional_iterator_tag,T,T,const T*,T>
 		{
 			public:
-				explicit iterator(wheel<T>& _wh,unsigned int _index=0);
+				friend class wheel;
 				
-				iterator& operator++();
-				iterator operator++(int);
-				iterator& operator--();
-				iterator operator--(int);
+				explicit iterator(wheel<T>& _wh,unsigned int _index=0) : wh(_wh),index(_index) {}
 				
-				friend iterator operator+(const iterator& lhs, int& rhs);
-				friend iterator operator-(const iterator& rhs, int& lhs);
+				iterator& operator++()
+				{
+					index=(index==wh.size()-1) ? 0 : index+1;
+					return *this;
+				}
 				
-				bool operator==(iterator other) const ;
-				bool operator!=(iterator other) const;
+				iterator operator++(int)
+				{
+					iterator retval = *this;
+					++(*this);
+					return retval;
+				}
 				
-				T& operator*() const;
+				iterator& operator--()
+				{
+					index=(!index) ? wh.size()-1 : index-1;
+					return *this;
+				}
+				
+				iterator operator--(int)
+				{
+					iterator retval = *this;
+					--(*this);
+					return retval;
+				}
+				
+				iterator operator+(const int& rhs)
+				{
+					return iterator(wh,index+=rhs);
+				}
+				
+				iterator operator-(const int& rhs)
+				{
+					return iterator(wh,index-=rhs);
+				}
+				
+				bool operator==(iterator other) const {return index==other.index;}
+				bool operator!=(iterator other) const {return !(*this==other);}
+				
+				T& operator*() const {return wh[index];}
 				
 				
 			private:
@@ -36,16 +66,31 @@ class wheel
 				unsigned int index;
 		};
 		
-		void push_back(T item);
-		void pop_back();
-		void erase(iterator it);
-		unsigned int size();
-		iterator begin();
-		iterator last();
-		T front();
-		T back();
+		void push_back(T item) {vec.push_back(item);}
 		
-		T& operator[] (const int index);
+		void pop_back() {vec.pop_back();}
+		
+		void erase(iterator it) {vec.erase( vec.begin()+it.index );}
+		
+		unsigned int size() {return vec.size();}
+		
+		iterator begin() {return iterator(*this);}
+		
+		iterator last() {return iterator(*this,vec.size()-1);}
+		
+		T front() {return vec.front();}
+		
+		T back() {return vec.back();}
+		
+		T& operator[] (const int index)
+		{
+			int num;
+			
+			if( abs(index) < static_cast<int>( vec.size() ) )num=index; //because I don't know how modular arithmetic works
+			else num=index%size();
+	
+			return (num<0) ? vec[vec.size()+num] : vec[num];
+		}
 		
 	private:
 		std::vector<T> vec;
