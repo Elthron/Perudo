9ce3bc7cdcc052a818f370df371681447bccc492
Ben <benjamin.stks@gmail.com>
2016-08-30 22:37:05 +0100

    now actually works

--- BasicAI.cpp
+++ BasicAI.cpp
@@ -1,18 +1,26 @@
+#include <cmath>
 #include "BasicAI.h"
 
-BasicAI(unsigned int _dice_size,std::vector<Bid*>& _bids):
+BasicAI::BasicAI(unsigned int _dice_size,std::list<Bid*>& _bids,unsigned int _total_dice):
 	Player(_dice_size),
-	bids(_bids)
+	bids(_bids),
+	total_dice(_total_dice)
 {}
 
-Bid* takeTurn(Bid last_bid)
+Bid* BasicAI::takeTurn(Bid* last_bid)
 {
-	//calculate highest value bid that can be made
+	//create the highest likely bid
+	Bid* new_bid=new Bid( std::floor(total_dice/6) ,1);
 	
-	//if current bid is lower, bid it
-	
-	//else challenge
+	if(*new_bid>*last_bid)return new_bid;
+	else return nullptr;
 }
 
-void sendBid(Bid bid)
+void BasicAI::sendBid(Bid*)
 {}
+
+unsigned int BasicAI::countDice(unsigned int value)
+{
+	--total_dice;
+	return Player::countDice(value);
+}
